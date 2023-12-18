673448e0f362d92c020b7683f43afe7cad6c1077
Emmanuel Nsikan-David <en56434@gmail.com>
2023-12-18 13:32:10 +0100

    cors url update

--- server/index.js
+++ server/index.js
@@ -14,12 +14,20 @@ const MONGO_URI = process.env.MONGO_URI
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 app.use(cookieParser())
-app.use(cors(
-    {
-        credentials : true,
-        origin : "http://localhost:5173"
+
+const whitelist = ["http://localhost:5173", "https://deliciouso.netlify.app/"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
     }
-))
+  },
+  credentials : true
+}
+
+app.use(cors(corsOptions))
 
 
 //routes
